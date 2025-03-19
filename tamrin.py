04c1dcfdb2b42435818a6e8b0bfd6446cde53f34
Amirmahdimanaseri <Amirmahdimanaseri@icloud.com>
2025-03-19 19:24:48 +0330

    Delete tamrin.py

--- tamrin.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-bot_token = "8199237450:AAHR6iOIFQWITKTKxSQjtR66oL5Nabbrr1w"  # 🔹 جایگزین کن با توکن خودت
-url = f"https://api.telegram.org/bot{bot_token}/getUpdates"
-
-response = requests.get(url)
-print(response.json())  # خروجی رو ببین
